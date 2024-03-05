e6e465067f3b3ee3dfe9328017581f1db29d348d
DPortella <d.portellaaraujo@gmail.com>
2024-03-04 21:49:55 -0300

    Atualizando

--- Semestre 2/Aula4/2.py
+++ Semestre 2/Aula4/2.py
@@ -1,20 +1,9 @@
-# Crie métodos especiais para a classe Automóvel
-
 class carro:
     def __init__(self):
-        self.nome = "Golf"
+        self.nome = "Golg"
         self.marca = "Volkswagen"
         self.peso = 500
         self.altura: 1.50
-
-    def __del__(self):
-        print("Objeto deletado!")
-
-    def __str__(self):
-        return(f"Essa é a classe com o nome: {self.nome}")
-
-    def __len__(self):
-        return len(self.__dict__)
 
     def dizer_nome(self):
         print(f"O nome do carro é {self.nome}")
@@ -25,10 +14,8 @@ class carro:
     def dizer_peso(self):
         print(f"O carro pesa {self.peso} quilos")
 
+
 car = carro()
 car.dizer_nome()
 car.dizer_marca()
 car.dizer_peso()
-print(f"A quantidade de atributos é de {len(car)}")
-print(car)
-del car
