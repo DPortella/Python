2b5801ead023551cf150c27590ec2a3b826a746d
Daniel Portella <68924608+DPortella@users.noreply.github.com>
2025-01-01 18:43:48 -0300

    Update and rename 1.py to Read_from_file.py

--- Semestre 2/Aula3/Read_from_file.py
+++ Semestre 2/Aula3/Read_from_file.py
@@ -1,4 +1,4 @@
-#Crie um programa que vai ler o arquivo ”Exercicio4.txt” caso não encontre deve cria-ló, com as informações das matérias\
+#Crie um programa que vai ler o arquivo ”Exercicio4.txt” caso não encontre deve criá-lo, com as informações das matérias\
 # que você tem no curso
 
 lst = ["Python, Architecture, Governança, Redes, Linux, Hardware"]
